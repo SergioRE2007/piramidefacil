f2a229a9e50a60ff4df0ff3f9113472e3f9e8d9e
Sergio Rodríguez <sergiore2007@gmail.com>
2025-10-31 14:30:45 +0100

    piramide numerica

--- src/piramide/piramide.java
+++ src/piramide/piramide.java
@@ -2,17 +2,26 @@ package piramide;
 
 public class piramide {
     public static void main(String[] args) {
-        int altura = 6;
+        int altura = 150;
+        int digitos = 0;
+        for (int aux = altura; aux > 0; aux = aux / 10) {
+            digitos++;
+        }
+        System.out.println(digitos);
 
-
-
-        for (int i = 0; i < altura; i++) {
+        for (int i = 0; i <= altura; i++) {
             for (int j = altura; j > i; j--) {
-                System.out.print(" ");
+                for (int k = digitos; k > 0; k--) {
+                    System.out.print(" ");
+                }
             }
-            for (int j = 0; j <= i*2; j++) {
-                System.out.print("*");
+            for (int j = i; j >= 1; j--) {
+                System.out.printf("%" + digitos + "d", j);
             }
+            for (int j = 2; j <= i; j++) {
+                System.out.printf("%" + digitos + "d", j);
+            }
+
             System.out.println();
         }
     }
